a8fe27bddd427d3bb5b6e6dd1aa8b7c76093a2d4
ganeshkanude8975 <ganeshkangude8975@gmail.com>
2024-11-30 14:51:37 +0530

    Update connection.js

--- routes/connection.js
+++ routes/connection.js
@@ -2,10 +2,10 @@ var mysql = require("mysql");
 var util = require("util");
 
 var conn = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"dica_an_dico"
+    host:"bmb61yyuqwfdfrajudor-mysql.services.clever-cloud.com",
+    user:"ug8xpsvp1h5nrsom",
+    password:"9jbtSNjDPlXLTApKuboA",
+    database:"bmb61yyuqwfdfrajudor"
 })
 
 var exe = util.promisify(conn.query).bind(conn);
